cce43b7fbc60ea8403ae1d2a944cea8fcaa5ec7e
serbus-create <serbus@v-h-s.cz>
2026-06-25 12:28:18 +0200

    Update oldmans-header.js

--- oldmans-header.js
+++ oldmans-header.js
@@ -241,7 +241,7 @@
       <div class="om-cat-menu-inner">
         <ul class="om-cat-list" id="omCatList">
           <li class="cat-favorite"><a href="/kategorie/bestseller/"><span class="cat-icon">⭐</span> Bestseller</a></li>
-          <li class="cat-sale"><a href="/stitky/akcni-cena/"><span class="cat-icon">🏷️</span> V akci</a></li>
+          <li class="cat-sale"><a href="/kategorie/v-akci/"><span class="cat-icon">🏷️</span> V akci</a></li>
           <li><a href="/kategorie/omacky-a-majonezy/">Omáčky a majonézy</a></li>
           <li><a href="/kategorie/salatove-dressingy/">Salátové dresingy</a></li>
           <li><a href="/kategorie/chilli-omacky/">Chilli omáčky</a></li>
