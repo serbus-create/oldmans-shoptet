eea4f3fa87abdc2a2cd76442a55c64abc9383eea
serbus-create <serbus@v-h-s.cz>
2026-07-14 15:09:25 +0200

    Update oldmans-header.js

--- oldmans-header.js
+++ oldmans-header.js
@@ -284,6 +284,13 @@
           <div class="om-login"><a href="/registrace/">👤</a></div>
           <div class="om-cart">
             <a href="/kosik/" class="cart-count toggle-window" data-target="cart" data-hover="true" data-redirect="true">
+              <span class="om-cart-icon">
+                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="9" cy="21" r="1"></circle>
+                  <circle cx="20" cy="21" r="1"></circle>
+                  <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                </svg>
+              </span>
               <span id="om-cart-price" class="cart-price">Prázdný košík</span>
               <span id="om-cart-badge" class="om-cart-badge">0</span>
             </a>
